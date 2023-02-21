3eb75750bbbe3f0710c921cc6caf4606d2cc7dfd
qrach <125902082+qrach@users.noreply.github.com>
2023-02-20 21:26:38 -0500

    Update Freebies.lua

--- Freebies.lua
+++ Freebies.lua
@@ -21,32 +21,37 @@ local MPS = game:GetService("MarketplaceService")
 
 Freebies["CheckGame"] = function(ID)
     if Freebies.Assets[ID] then
-    	local AllOwned = true
-        for _,Asset in pairs(Freebies.Assets[ID]) do
-            local Owned = MPS:PlayerOwnsAsset(LocalPlayer,Asset)
+        local AllOwned = true
+        for _, Asset in pairs(Freebies.Assets[ID]) do
+            local Owned = MPS:PlayerOwnsAsset(LocalPlayer, Asset)
             if not Owned then
-            	AllOwned = false
-            	break
+                AllOwned = false
+                break
             end
         end
         if AllOwned then
-			local Idx = table.find(Freebies.Assets,ID)
-			for Idx2, _ in pairs(Freebies.Assets) do
-				if Idx2 == Freebies.Assets[Idx+1] then
-					game:GetService("TeleportService"):Teleport(Idx2, LocalPlayer)
-					repeat task.wait() until not true
-					break
-				end
-			end
-		end
+            local Idx
+            for i, v in pairs(Freebies.Assets) do
+                if i == ID then
+                    Idx = i
+                    break
+                end
+            end
+            for i, _ in pairs(Freebies.Assets) do
+                if i == Freebies.Assets[Idx + 1] then
+                    game:GetService("TeleportService"):Teleport(i, LocalPlayer)
+                    break
+                end
+            end
+        end
     else
-		for Idx,_ in pairs(Freebies.Assets) do
-			game:GetService("TeleportService"):Teleport(Idx, LocalPlayer)
-			repeat task.wait() until not true
-			break
-		end
+        for i, _ in pairs(Freebies.Assets) do
+            game:GetService("TeleportService"):Teleport(i, LocalPlayer)
+            break
+        end
     end
 end
+
 
 if Freebies.AutoQueue then
     queue_on_teleport("getgenv().Freebies={[\"WebStuff\"]=false}; "..game:HttpGet("https://raw.githubusercontent.com/qrach/RBXProjects/main/Freebies.lua"))
