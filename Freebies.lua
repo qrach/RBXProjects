ba6e3b0425df5cd7c26b9f87c5848f5537e39301
qrach <125902082+qrach@users.noreply.github.com>
2023-02-20 19:24:34 -0500

    Update Freebies.lua

--- Freebies.lua
+++ Freebies.lua
@@ -43,27 +43,103 @@ end
 repeat task.wait() until game:IsLoaded()
 if game.PlaceId == 11369456293 then
     local LocalPlayer = game:GetService("Players").LocalPlayer
-    local PingData = game:GetService("Stats").Network.ServerStatsItem["Data Ping"]
     local Character = LocalPlayer.Character or LocalPlayer.CharacterAdded:wait()
+    LocalPlayer.CharacterAdded:Connect(function(NewCharacter)
+        Character = NewCharacter
+    end)
     repeat task.wait() until Character:FindFirstChildOfClass("Humanoid") and Character.PrimaryPart
-    Character:SetPrimaryPartCFrame(workspace.CheckPointSpawns.checkPoint1.CFrame)
-    task.wait(PingData:GetValue()/250)
-    Character:SetPrimaryPartCFrame(workspace.CheckPointSpawns.checkPoint2.CFrame)
-    task.wait(PingData:GetValue()/250)
-    Character:SetPrimaryPartCFrame(workspace.CheckPointSpawns.End.CFrame)
-    task.wait(PingData:GetValue()/250)
+    Character:SetPrimaryPartCFrame(CFrame.new(workspace.Zones.checkPoint1.CFrame.Position))
+    repeat task.wait() until workspace.CheckPointSpawns.checkPoint1.CheckPointReached.Enabled
+    Character:SetPrimaryPartCFrame(CFrame.new(workspace.Zones.checkPoint2.CFrame.Position))
+    repeat task.wait() until workspace.CheckPointSpawns.checkPoint2.CheckPointReached.Enabled
+    Character:SetPrimaryPartCFrame(CFrame.new(workspace.Zones.End.CFrame.Position))
+    repeat task.wait() until workspace.CheckPointSpawns.End.CheckPointReached.Enabled
+    Character.PrimaryPart.Anchored = true
+    repeat task.wait() until LocalPlayer.PlayerGui.FadeGui.Fade.Transparency ~= 1
+    repeat task.wait() until LocalPlayer.PlayerGui.FadeGui.Fade.Transparency == 1
     local Queue = workspace.FutureBall.Queue.FutureBallQueue.UI.PlayerBoardOrange.Info.Q
     repeat task.wait() until not string.find(Queue.Text,"4/4") and not string.find(Queue.Text,"Starting")
-    local Con
-    local Map
-    Con = workspace.MinigameBin.ChildAdded:Connect(function(Added)
-        Map = Added
-        repeat task.wait() until LocalPlayer.PlayerGui.ChooseTeamUI.Enabled
-        game:GetService("ReplicatedStorage").Packages["_Index"]["sleitnick_knit@1"]["4"]["2"].knit.Services.FutureBallGameService.RE.TeamChange:FireServer(false)
-        Con:Disconnect()
+    local Wins = 0
+    local Scores = 0
+    local Games = 0
+    TargetWins = 1
+    TargetScores = 10
+    TargetGames = 3
+    local WinGui = LocalPlayer.PlayerGui.WinnerGui
+    local ScoreGui = LocalPlayer.PlayerGui.ScoreGui
+    WinGui:GetPropertyChangedSignal("Enabled"):Connect(function()
+        wait(.25)
+        if WinGui.Enabled and WinGui:WaitForChild("Container"):WaitForChild("Title").Text == "WINNER" then
+            Wins = Wins+1
+        end
+    end)
+    ScoreGui:GetPropertyChangedSignal("Enabled"):Connect(function()
+        task.wait(.25)
+        if ScoreGui.Enabled and ScoreGui:WaitForChild("Container"):WaitForChild("Description2").Text:find(LocalPlayer.DisplayName.." scored!") then
+            Scores = Scores+1
+        end
+    end)
+    LocalPlayer.PlayerGui.ChooseTeamUI:GetPropertyChangedSignal("Enabled"):Connect(function()
+    	LocalPlayer.PlayerGui.ChooseTeamUI.Enabled = false
     end)
     repeat
-        Character:SetPrimaryPartCFrame(workspace.FutureBall.Queue.FutureBallQueue.Base.CFrame)
-        task.wait()
-    until Map
+		Games = Games+1
+        local Con1
+        local Map
+        Con1 = workspace.MinigameBin.ChildAdded:Connect(function(Added1)
+            Map = Added1
+        end)
+        local Con2
+        local OpNum
+        Con2 = Character.DescendantAdded:Connect(function(Added2)
+             if Added2.Name == "BlueAtt" then
+                OpNum = 1
+                Con1:Disconnect()
+                Con2:Disconnect()
+            elseif Added2.Name == "OrangeAtt" then
+                OpNum = 2
+                Con1:Disconnect()
+                Con2:Disconnect()
+            end
+        end)
+        repeat
+			if not OpNum then
+				Character.PrimaryPart.Anchored = false
+				repeat
+					Character:SetPrimaryPartCFrame(workspace.FutureBall.Queue.FutureBallQueue.Base.CFrame+Vector3.new(0,1,0))
+					task.wait()
+				until Map
+				Character:SetPrimaryPartCFrame(workspace.FutureBall.Queue.FutureBallQueue.Base.CFrame+Vector3.new(0,100000,0))
+				Character.PrimaryPart.Anchored = true
+				local Done = false
+				task.spawn(function()
+					task.wait(25)
+					Done = true
+				end)
+				repeat task.wait() until Done or OpNum
+			end
+		until OpNum
+        local Goal = Map:WaitForChild("Court"):WaitForChild("Goals"):WaitForChild("T"..tostring(OpNum).."Goal")
+        local BallCon
+        BallCon = Map.Court.BallBin.ChildAdded:Connect(function(Ball)
+            
+         	wait(.5)
+         	Character.PrimaryPart.Anchored = false
+        	repeat
+				if Scores < TargetScores or Wins < TargetWins then
+					local FurthestPoint = -((Goal.Position - Ball.Position).Unit) * (Ball.Size/2) + Ball.Position
+					local VelDirection = (FurthestPoint - Goal.Position).Unit
+					Character:SetPrimaryPartCFrame(CFrame.new(FurthestPoint))
+					Character.PrimaryPart.Velocity = VelDirection*50
+				end
+	    		task.wait()
+        	until Ball.Parent ~= Map.Court.BallBin or LocalPlayer.PlayerGui.WinnerGui.Enabled
+        	Character.PrimaryPart.Anchored = true
+        	if LocalPlayer.PlayerGui.WinnerGui.Enabled or (Scores >= TargetScores and Wins >= TargetWins) then
+        		BallCon:Disconnect()
+			end
+	    end)
+        repeat task.wait() until LocalPlayer.PlayerGui.WinnerGui.Enabled
+    until Wins >= TargetWins and Scores >= TargetScores and Games >= TargetGames
+    Character.PrimaryPart.Anchored = false
 end
